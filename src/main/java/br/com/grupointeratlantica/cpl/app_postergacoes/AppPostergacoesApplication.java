8dd82aa841c4313905b8b4ab477bb33abe1599be
devopMarkz <marcosdev2002@gmail.com>
2025-01-29 17:07:54 -0300

    First commit

---
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/AppPostergacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupointeratlantica.cpl.app_postergacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPostergacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPostergacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupointeratlantica/cpl/app_postergacoes/AppPostergacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupointeratlantica.cpl.app_postergacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPostergacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
