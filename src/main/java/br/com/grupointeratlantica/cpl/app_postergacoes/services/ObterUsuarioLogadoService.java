7b8d51cc78c28ede2168b4f5b2ffed3de9a69579
devopMarkz <marcosdev2002@gmail.com>
2025-02-01 22:16:09 -0300

    Criando servico para obter usuario logado

---
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/ObterUsuarioLogadoService.java
@@ -0,0 +1,19 @@
+package br.com.grupointeratlantica.cpl.app_postergacoes.services;
+
+import br.com.grupointeratlantica.cpl.app_postergacoes.models.Usuario;
+import br.com.grupointeratlantica.cpl.app_postergacoes.repositories.UsuarioRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ObterUsuarioLogadoService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario obterUsuario(){
+        String login = SecurityContextHolder.getContext().getAuthentication().getName();
+        return usuarioRepository.findByEmail(login).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+    }
+
+}
