5b37b0751375f0c2dcc0ee79da37d9eb74f45302
devopMarkz <marcosdev2002@gmail.com>
2025-02-05 09:41:01 -0300

    Criando servico de envio de email

--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/EmailService.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/EmailService.java
@@ -1,4 +1,11 @@
 package br.com.grupointeratlantica.cpl.app_postergacoes.services;
 
+import br.com.grupointeratlantica.cpl.app_postergacoes.models.NotaPostergada;
+
+import java.util.List;
+
 public interface EmailService {
+
+    void enviarEmailComNotas(List<NotaPostergada> notasPostergadas, List<String> emailsCorporativos);
+
 }
--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/impl/EmailServiceImpl.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/impl/EmailServiceImpl.java
@@ -1,10 +1,65 @@
 package br.com.grupointeratlantica.cpl.app_postergacoes.services.impl;
 
+import br.com.grupointeratlantica.cpl.app_postergacoes.models.NotaPostergada;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.EmailService;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.RelatorioExcelService;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.EmailException;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.GeracaoDeRelatorioInvalidaException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 @Service
-public class EmailServiceImpl {
+public class EmailServiceImpl implements EmailService {
 
+    @Autowired
+    private JavaMailSender javaMailSender;
 
+    @Autowired
+    private RelatorioExcelService relatorioExcelService;
+
+    @Value("${spring.mail.username}")
+    private String fromEmailId;
+
+    @Override
+    @Async
+    public void enviarEmailComNotas(List<NotaPostergada> notasPostergadas, List<String> emailsCorporativos) {
+        String[] destinatarios = new String[emailsCorporativos.size()];
+
+        for (int i = 0; i < destinatarios.length; i++) {
+            destinatarios[i] = emailsCorporativos.get(i);
+        }
+
+        String assunto = "Relatório de notas postergadas - " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String corpo = "Segue anexo o relatório de notas postergadas.";
+
+        try {
+            File relatorio = relatorioExcelService.gerarRelatorioNotas(notasPostergadas);
+
+            MimeMessage mensagem = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mensagem, true);
+            helper.setTo(destinatarios);
+            helper.setSubject(assunto);
+            helper.setText(corpo);
+
+            helper.addAttachment(LocalDate.now() + " Relatorio_Notas.xlsx", new FileSystemResource(relatorio));
+
+            javaMailSender.send(mensagem);
+        } catch (MessagingException e){
+            throw new EmailException("Erro no envio de e-mail.");
+        }
+    }
 
 }
