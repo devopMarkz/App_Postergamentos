0996d40b182660e314e9e6a6e8a03a4d82cc6bab
devopMarkz <marcosdev2002@gmail.com>
2025-02-05 09:44:31 -0300

    Criando metodo para chamar o servico de envio de email

--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/NotaPostergadaService.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/NotaPostergadaService.java
@@ -1,4 +1,24 @@
 package br.com.grupointeratlantica.cpl.app_postergacoes.services;
 
+import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.notaPostergada.NotaPostergadaCriacaoDTO;
+import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.notaPostergada.NotaPostergadaDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
 public interface NotaPostergadaService {
+
+    NotaPostergadaDTO salvar(NotaPostergadaCriacaoDTO notaPostergadaCriacaoDTO);
+
+    Page<NotaPostergadaDTO> buscarTodasPorFiltro(LocalDate dataMinima, LocalDate dataMaxima, Long numeroUnico, String numeroNota, Integer codigoEmpresa, Pageable pageable);
+
+    Page<NotaPostergadaDTO> buscarPorEmpresas(LocalDate dataMinima, LocalDate dataMaxima, Pageable pageable);
+
+    void atualizar(NotaPostergadaCriacaoDTO notaPostergadaCriacaoDTO);
+
+    void deletarPorNumeroUnico(Long numeroUnico);
+
+    void enviarTodasAsNotasPostergadas();
+
 }
--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/impl/NotaPostergadaServiceImpl.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/services/impl/NotaPostergadaServiceImpl.java
@@ -2,16 +2,17 @@ package br.com.grupointeratlantica.cpl.app_postergacoes.services.impl;
 
 import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.notaPostergada.NotaPostergadaCriacaoDTO;
 import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.notaPostergada.NotaPostergadaDTO;
+import br.com.grupointeratlantica.cpl.app_postergacoes.models.Empresa;
 import br.com.grupointeratlantica.cpl.app_postergacoes.models.NotaPostergada;
 import br.com.grupointeratlantica.cpl.app_postergacoes.models.Usuario;
+import br.com.grupointeratlantica.cpl.app_postergacoes.models.enums.StatusNotificacao;
 import br.com.grupointeratlantica.cpl.app_postergacoes.repositories.EmpresaRepository;
 import br.com.grupointeratlantica.cpl.app_postergacoes.repositories.NotaPostergadaRepository;
 import br.com.grupointeratlantica.cpl.app_postergacoes.repositories.UsuarioRepository;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.EmailService;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.NotaPostergadaService;
 import br.com.grupointeratlantica.cpl.app_postergacoes.services.ObterUsuarioLogadoService;
-import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.EmpresaInexistenteException;
-import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.NotaPostergadaInexistenteException;
-import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.NotaPostergadaJaExistenteException;
-import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.UsuarioInexistenteException;
+import br.com.grupointeratlantica.cpl.app_postergacoes.services.exceptions.*;
 import br.com.grupointeratlantica.cpl.app_postergacoes.utils.NotaPostergadaMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,24 +21,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
 
 @Service
-public class NotaPostergadaServiceImpl {
+public class NotaPostergadaServiceImpl implements NotaPostergadaService {
 
     private NotaPostergadaRepository notaPostergadaRepository;
     private UsuarioRepository usuarioRepository;
     private EmpresaRepository empresaRepository;
     private NotaPostergadaMapper notaPostergadaMapper;
     private ObterUsuarioLogadoService obterUsuarioLogadoService;
+    private EmailService emailService;
 
-    public NotaPostergadaServiceImpl(NotaPostergadaRepository notaPostergadaRepository, UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository, NotaPostergadaMapper notaPostergadaMapper, ObterUsuarioLogadoService obterUsuarioLogadoService) {
+    public NotaPostergadaServiceImpl(NotaPostergadaRepository notaPostergadaRepository, UsuarioRepository usuarioRepository, EmpresaRepository empresaRepository, NotaPostergadaMapper notaPostergadaMapper, ObterUsuarioLogadoService obterUsuarioLogadoService, EmailService emailService) {
         this.notaPostergadaRepository = notaPostergadaRepository;
         this.usuarioRepository = usuarioRepository;
         this.empresaRepository = empresaRepository;
         this.notaPostergadaMapper = notaPostergadaMapper;
         this.obterUsuarioLogadoService = obterUsuarioLogadoService;
+        this.emailService = emailService;
     }
 
+    @Override
     @Transactional
     public NotaPostergadaDTO salvar(NotaPostergadaCriacaoDTO notaPostergadaCriacaoDTO){
         if(notaPostergadaRepository.existsByNumeroUnico(notaPostergadaCriacaoDTO.numeroUnico())) {
@@ -48,12 +54,14 @@ public class NotaPostergadaServiceImpl {
         return notaPostergadaMapper.toDTO(novaNotaPostergada);
     }
 
+    @Override
     @Transactional(readOnly = true)
     public Page<NotaPostergadaDTO> buscarTodasPorFiltro(LocalDate dataMinima, LocalDate dataMaxima, Long numeroUnico, String numeroNota, Integer codigoEmpresa, Pageable pageable){
         Page<NotaPostergada> notasPostergadas = notaPostergadaRepository.pesquisarNotasPorFiltros(dataMinima, dataMaxima, numeroUnico, numeroNota, codigoEmpresa, pageable);
         return notasPostergadas.map(notaPostergada -> notaPostergadaMapper.toDTO(notaPostergada));
     }
 
+    @Override
     @Transactional(readOnly = true)
     public Page<NotaPostergadaDTO> buscarPorEmpresas(LocalDate dataMinima, LocalDate dataMaxima, Pageable pageable){
         String login = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -66,6 +74,7 @@ public class NotaPostergadaServiceImpl {
         return notasPostergadas.map(notaPostergada -> notaPostergadaMapper.toDTO(notaPostergada));
     }
 
+    @Override
     @Transactional
     public void atualizar(NotaPostergadaCriacaoDTO notaPostergadaCriacaoDTO){
         NotaPostergada notaPostergada = notaPostergadaRepository.findByNumeroUnico(notaPostergadaCriacaoDTO.numeroUnico())
@@ -74,10 +83,30 @@ public class NotaPostergadaServiceImpl {
         notaPostergadaRepository.save(notaAtualizada);
     }
 
+    @Override
     @Transactional
     public void deletarPorNumeroUnico(Long numeroUnico){
         if(!notaPostergadaRepository.existsByNumeroUnico(numeroUnico)) throw new NotaPostergadaInexistenteException("Postergamento inexistente.");
         notaPostergadaRepository.deleteByNumeroUnico(numeroUnico);
+    }
+
+    @Override
+    @Transactional
+    public void enviarTodasAsNotasPostergadas(){
+        List<NotaPostergada> notasPostergadas = notaPostergadaRepository.obterNotasPendentesDeEnvio();
+
+        if(notasPostergadas.isEmpty()){
+            throw new EmailException("Não há notas pendentes de envio!");
+        }
+
+        List<String> emailsCorporativos = empresaRepository.findAll().stream().map(Empresa::getEmailCorporativo).toList();
+
+        emailService.enviarEmailComNotas(
+                notasPostergadas.stream().sorted(Comparator.comparing(o -> o.getEmpresa().getNome())).toList(),
+                emailsCorporativos
+        );
+
+        notasPostergadas.forEach(notaPostergada -> notaPostergada.setStatusNotificacao(StatusNotificacao.ENVIADO));
     }
 
     private NotaPostergada atualizarNota(NotaPostergada notaPostergada, NotaPostergadaCriacaoDTO notaPostergadaCriacaoDTO){
