e91209554a25e493a204f0303687c4a8f6e6ac2f
devopMarkz <marcosdev2002@gmail.com>
2025-02-06 20:47:43 -0300

    Preparando o projeto para Deploy

---
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.grupointeratlantica.cpl.app_postergacoes.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+
+    @Value("${cors.origins}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+            }
+        };
+    }
+
+}
