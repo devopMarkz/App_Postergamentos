dbd431300aa854181c2a508d36a9621f6ce44952
devopMarkz <marcosdev2002@gmail.com>
2025-02-09 23:38:36 -0300

    Documentando os endpoints de autenticacao e usuarios

--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/controllers/AuthController.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/controllers/AuthController.java
@@ -3,6 +3,10 @@ package br.com.grupointeratlantica.cpl.app_postergacoes.controllers;
 import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.login.AuthDTO;
 import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.token.TokenDTO;
 import br.com.grupointeratlantica.cpl.app_postergacoes.services.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Autenticação de Usuário")
 public class AuthController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthController.class);
@@ -28,6 +33,11 @@ public class AuthController {
     }
 
     @PostMapping
+    @Operation(summary = "Autenticar usuário",
+    description = "Informe usuário e senha para obter um token JWT e utilizá-lo para efetuar requisições no sistema.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário autenticado.")
+    })
     public ResponseEntity<TokenDTO> autenticarUsuario(@RequestBody AuthDTO authDTO, HttpServletRequest request) {
         log.info("Requisição recebida: Método={} | Endpoint={} | Usuário={}",
                 request.getMethod(), request.getRequestURI(), authDTO.login().substring(0, 3) + "***");
--- src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/controllers/UsuarioController.java
+++ src/main/java/br/com/grupointeratlantica/cpl/app_postergacoes/controllers/UsuarioController.java
@@ -5,11 +5,14 @@ import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.usuario.UsuarioCriac
 import br.com.grupointeratlantica.cpl.app_postergacoes.dtos.usuario.UsuarioRespostaDTO;
 import br.com.grupointeratlantica.cpl.app_postergacoes.services.UsuarioService;
 import br.com.grupointeratlantica.cpl.app_postergacoes.services.impl.UsuarioServiceImpl;
-import br.com.grupointeratlantica.cpl.app_postergacoes.utils.GeradorDeURI;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,7 @@ import static br.com.grupointeratlantica.cpl.app_postergacoes.utils.GeradorDeURI
 
 @RestController
 @RequestMapping("/usuarios")
+@Tag(name = "Endpoints de Usuários", description = "Permite cadastrar, visualizar e atualizar usuários.")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
@@ -27,6 +31,11 @@ public class UsuarioController {
     }
 
     @PostMapping
+    @Operation(summary = "Cadastrar usuário.", description = "Endpoint público que permite a criação de um novo usuário no sistema.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário criado."),
+            @ApiResponse(responseCode = "400", description = "Erro de entrada de dados")
+    })
     public ResponseEntity<Void> criarUsuario(@Valid @RequestBody UsuarioCriacaoDTO usuarioCriacaoDTO){
         var usuario = usuarioService.salvar(usuarioCriacaoDTO);
         return ResponseEntity.created(gerarURI(usuario.id())).build();
@@ -34,12 +43,18 @@ public class UsuarioController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMINISTRADOR')")
-    public ResponseEntity<Page<UsuarioRespostaDTO>> buscarUsuarios(@PageableDefault(page = 0, size = 10) Pageable pageable){
+    @Operation(summary = "Visualizar usuários.", description = "Permite que administradores visualizem todos os usuários do sistema.")
+    public ResponseEntity<Page<UsuarioRespostaDTO>> buscarUsuarios(
+            @RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize
+    ){
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
         Page<UsuarioRespostaDTO> usuarios = usuarioService.buscarTodos(pageable);
         return ResponseEntity.ok(usuarios);
     }
 
     @PutMapping
+    @Operation(summary = "Atualizar senha.", description = "Permite atualizar a senha do usuário.")
     public ResponseEntity<Void> atualizarSenha(@Valid @RequestBody UsuarioAtualizacaoDTO usuarioAtualizacaoDTO){
         usuarioService.atualizarUsuario(usuarioAtualizacaoDTO);
         return ResponseEntity.noContent().build();
